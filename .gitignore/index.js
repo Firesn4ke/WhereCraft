9e8a7435a27a2f8e435e6636bbaaa2de3aa58b06
Firesn4ke <47301311+Firesn4ke@users.noreply.github.com>
2019-04-05 11:04:00 +0200

    Add files via upload

---
+++ .gitignore/index.js
@@ -0,0 +1,65 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+
+let prefix = "!"
+
+bot.login(process.env.TOKEN);
+        
+bot.on('message', function (message) {
+    if (message.content === '!help') {
+        message.channel.send(' Les Commandes sont disponibles avec le préfix **!** : ')
+        message.channel.send(' **site**, **shop**, **regle**, **cgv** ')
+            
+    }
+ })
+
+bot.on('guildMemberAdd', member => {
+    member.guild.channels.get('146281705949364224').send(' Bienvenue ' + member.user + ' dans la Secte. ')
+    member.guild.channels.get('146281705949364224').send('Nous sommes désormais ' + member.guild.memberCount + ' personnes ;)' );
+ })
+
+bot.on('guildMemberRemove', member => {
+   member.guild.channels.get('146281705949364224').send(' En Revoir ' + member.user + ' en dehors de la Secte.' );
+})
+
+bot.on('message', function (message) {
+    if (message.content === '!site') {
+        message.channel.send('https://www.wherecraft.eu')
+    }
+ })
+
+bot.on('message', function (message) {
+    if (message.content === '!shop') {
+        message.channel.send('https://www.wherecraft.eu/shop')
+    }
+ })
+
+bot.on('message', function (message) {
+    if (message.content === '!regle') {
+        message.channel.send('https://www.wherecraft.eu/p/regles')
+    }
+ })
+
+bot.on('message', function (message) {
+    if (message.content === '!cgv') {
+        message.channel.send('https://wherecraft.eu/p/cgu-cgv')
+    }
+ })
+
+
+bot.on('message', function (message) {
+    if (message.content === '!numberplayer') {
+        message.channel.send( 'Il y a ' + message.guild.memberCount + ' joueurs sur le serveur Discord ;)')
+    }
+ })
+
+ bot.on('ready', function () {
+    bot.user.setActivity('Commande : !help').catch(console.error)
+ })
+
+ bot.on('guildMemberAdd', function (member) {
+    member.createDM().then(function (channel) {
+        return channel.send('Bienvenue dans la secte de **WhereCraft** ! Bienvenue parmis nous ' + member.displayName )
+ 
+    }).catch(console.error)
+ })
