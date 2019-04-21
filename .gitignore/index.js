5a93657dc9312fb898eae728978ba8acd054d69b
Firesn4ke <47301311+Firesn4ke@users.noreply.github.com>
2019-04-21 22:37:12 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -16,7 +16,8 @@ bot.on('message', function (message) {
 bot.on('guildMemberAdd', member => {
     member.guild.channels.get('146281705949364224').send(' Bienvenue ' + member.user + ' dans la Secte. ')
     member.guild.channels.get('146281705949364224').send('Nous sommes désormais ' + member.guild.memberCount + ' personnes :wink: ' );
-    member.addRole('569594186165256192')    
+    member.addRole('569618227227787299')  
+    member.addRole('569594186165256192') 
  })
 
 bot.on('guildMemberRemove', member => {
