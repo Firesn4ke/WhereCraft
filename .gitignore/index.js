cdddadaa0c7aafe47f3afb3ac25a281d4086aed4
Firesn4ke <47301311+Firesn4ke@users.noreply.github.com>
2019-04-22 22:55:01 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,6 +1,8 @@
 const { Client, RichEmbed } = require('discord.js');
 const bot = new Client({ disableEveryone: true })
 
+bot.login(process.env.TOKEN);
+
  bot.on('guildMemberAdd', member => {
     member.addRole('569664315460943872')
  })
