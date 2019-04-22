7e89e2062e7f9d361767f9173db34fc7b94639da
Firesn4ke <47301311+Firesn4ke@users.noreply.github.com>
2019-04-22 22:53:00 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,7 +2,5 @@ const { Client, RichEmbed } = require('discord.js');
 const bot = new Client({ disableEveryone: true })
 
  bot.on('guildMemberAdd', member => {
-    member.guild.channels.get('554081631279906827').send(' Bienvenue ' + member.user + ' dans la Secte. ')
-    member.guild.channels.get('554081631279906827').send('Nous sommes désormais ' + member.guild.memberCount );
     member.addRole('569664315460943872')
  })
