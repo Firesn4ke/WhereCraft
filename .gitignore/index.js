06aee1a8820a4e698779f2e3bcca96aeb90c36f9
Firesn4ke <47301311+Firesn4ke@users.noreply.github.com>
2019-04-22 22:35:35 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,65 +3,6 @@ const bot = new Client({ disableEveryone: true })
 
 bot.login(process.env.TOKEN);
 
-bot.on('message', function (message) {
-    if (message.content === '!help') {
-        message.channel.send(' Les Commandes sont disponibles avec le préfix **!** : ')
-        message.channel.send(' **site**, **shop**, **regle**, **cgv** ')
-            
-    }
- })
-
-bot.on('guildMemberAdd', member => {
-    member.addRole('569664315460943872')
- })
-
-
-bot.on('guildMemberRemove', member => {
-   member.guild.channels.get('146281705949364224').send(' En Revoir ' + member.user + ' en dehors de la Secte.' );
-})
-
-bot.on('message', function (message) {
-    if (message.content === '!site') {
-        message.channel.send('https://www.wherecraft.eu')
-    }
- })
-
-bot.on('message', function (message) {
-    if (message.content === '!shop') {
-        message.channel.send('https://www.wherecraft.eu/shop')
-    }
- })
-
-bot.on('message', function (message) {
-    if (message.content === '!regle') {
-        message.channel.send('https://www.wherecraft.eu/p/regles')
-    }
- })
-
-bot.on('message', function (message) {
-    if (message.content === '!cgv') {
-        message.channel.send('https://wherecraft.eu/p/cgu-cgv')
-    }
- })
-
-
-bot.on('message', function (message) {
-    if (message.content === '!numberplayer') {
-        message.channel.send( 'Il y a ' + message.guild.memberCount + ' joueurs sur le serveur Discord ;)')
-    }
- })
-
- bot.on('ready', function () {
-    bot.user.setActivity('Commande : !help').catch(console.error)
- })
-
- bot.on('guildMemberAdd', function (member) {
-    member.createDM().then(function (channel) {
-        return channel.send('Bienvenue dans la secte de **WhereCraft** ! Bienvenue parmis nous ' + member.displayName )
- 
-    }).catch(console.error)
- })
-
 
 // Configuration & Settings
 const yourID = "413334310561513472"; //Instructions on how to get this: https://redd.it/40zgse
@@ -191,3 +132,63 @@ bot.on('raw', async event => {
         }
     }
 });
+
+bot.on('message', function (message) {
+    if (message.content === '!help') {
+        message.channel.send(' Les Commandes sont disponibles avec le préfix **!** : ')
+        message.channel.send(' **site**, **shop**, **regle**, **cgv** ')
+            
+    }
+ })
+
+bot.on('guildMemberAdd', member => {
+    member.addRole('569664315460943872')
+ })
+
+
+bot.on('guildMemberRemove', member => {
+   member.guild.channels.get('146281705949364224').send(' En Revoir ' + member.user + ' en dehors de la Secte.' );
+})
+
+bot.on('message', function (message) {
+    if (message.content === '!site') {
+        message.channel.send('https://www.wherecraft.eu')
+    }
+ })
+
+bot.on('message', function (message) {
+    if (message.content === '!shop') {
+        message.channel.send('https://www.wherecraft.eu/shop')
+    }
+ })
+
+bot.on('message', function (message) {
+    if (message.content === '!regle') {
+        message.channel.send('https://www.wherecraft.eu/p/regles')
+    }
+ })
+
+bot.on('message', function (message) {
+    if (message.content === '!cgv') {
+        message.channel.send('https://wherecraft.eu/p/cgu-cgv')
+    }
+ })
+
+
+bot.on('message', function (message) {
+    if (message.content === '!numberplayer') {
+        message.channel.send( 'Il y a ' + message.guild.memberCount + ' joueurs sur le serveur Discord ;)')
+    }
+ })
+
+ bot.on('ready', function () {
+    bot.user.setActivity('Commande : !help').catch(console.error)
+ })
+
+ bot.on('guildMemberAdd', function (member) {
+    member.createDM().then(function (channel) {
+        return channel.send('Bienvenue dans la secte de **WhereCraft** ! Bienvenue parmis nous ' + member.displayName )
+ 
+    }).catch(console.error)
+ })
+
