34d831802d7f5348397615dc9aee944019ca1270
Firesn4ke <47301311+Firesn4ke@users.noreply.github.com>
2019-06-08 21:53:10 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -11,18 +11,6 @@ bot.on('message', function (message) {
     }
  })
 
-module.exports.run = async(client, message, args) => {
-
-    const membre = message.mentions.users.first() || message.author;
-
-    var embed = new Discord.RichEmbed()
-    .setTitle(`Avatare de **${membre.username}**`)
-    .setDescription(`[Télécharger](${membre.displayAvatarURL})`)
-    .setImage(membre.displayAvatarURL)
-
-    message.channel.send(embed)
-    message.delete().catch(O_o=>{});
-
 };
 
 module.exports.help = {
